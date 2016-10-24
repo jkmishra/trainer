8c9aab1173820f20f141af4b5995a4d005a9aecb
jkmishra <jugulmishra2007@gmail.com>
2016-10-24 18:50:06 +0530

    trainer response file

---
+++ src/main/java/com/tavant/trainer/TrainResponseData.java
@@ -0,0 +1,24 @@
+package com.tavant.trainer;
+
+import java.util.List;
+
+public class TrainResponseData {
+	private int statusCode;
+	private List<String> response;
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public List<String> getResponse() {
+		return response;
+	}
+	public void setResponse(List<String> response) {
+		this.response = response;
+	}
+	@Override
+	public String toString() {
+		return "TrainResponseData [statusCode=" + statusCode + ", response=" + response + "]";
+	}
+}
